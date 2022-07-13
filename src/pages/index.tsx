8e6d413d276c70911ca36075726d8612a0761589
acatzk <joshuaimalay@gmail.com>
2022-07-13 11:19:31 +0800

    feat: install tailwindcss for ui

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Layout from '~/layouts/Layout'
+
+const Index: NextPage = () => {
+  return (
+    <Layout>
+      <h1 className='text-2xl font-semibold text-blue-500'>This is my initial pages with tailwindcss</h1>
+    </Layout>
+  )
+}
+
+export default Index
